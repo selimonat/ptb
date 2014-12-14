8ad69a94624eff80ce62c08e59ae05f1a4e7bbaa
selimonat <onatselim@gmail.com>
2014-12-14 15:48:23 +0100

    Few basic routines written with psychophysics toolbox suitable for receiving pulses from the serial buffer. I used these during an fMRI experiment to communicate with the so-called cogent box.

---
+++ WaitForPulseZero.m
@@ -0,0 +1,23 @@
+function [t_pulse,value]=WaitForPulseZero(port)
+%[t_pulse,value]=WaitForPulseZero(port,n)
+
+
+
+value   = [];
+t_now   = GetSecs;%this is the time this function is called, we require that the pulse
+%which is detected to arrive later than this time.
+
+t_pulse = t_now;%if n=0, that is no wait for pulse, than pulse arrival is now...
+
+ok =1;
+while ok
+    %read data with a granularity of 2 in a blocking way...
+    [value, t_pulse] = IOPort('Read', port,1,2);
+    if (t_pulse > t_now) && sum(value) == 0%only consider pulses that arrives strictly after the call of this function.
+        ok = 0;
+        %Log(t_pulse,0,NaN);%log the scan onset, this is done
+        %outside
+    end
+end
+fprintf('Pulse Detected at %g\nValue is [%d %d]\n',t_pulse,value(1),value(2));
+
