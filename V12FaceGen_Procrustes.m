bb337b1741dfee087c52d657e839d1b7029bb266
leakp <lea.kampermann@gmail.com>
2015-03-11 17:11:12 +0100

    GWTW and V12 Facegen_Procrustes

--- V12FaceGen_Procrustes.m
+++ V12FaceGen_Procrustes.m
@@ -2,8 +2,7 @@ function [D,Z,T] = V12FaceGen_Procrustes(v1_space,save_path)
 %[D,Z,T] = V12FaceGen_Procrustes(v1_space)
 
 facespace = GetFaceSpace;
-[D, Z, T] = procrustes(v1_space,facespace);
-facespace=GetFaceSpace;
+[D, Z, T] = procrustes(v1_space,facespace,'reflection',0);
 figure
 plot(facespace(:,1),facespace(:,2),'b')
 hold on;
