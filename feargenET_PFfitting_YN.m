8f0dd598205d25a2bc7652be1286a1517bb62536
leakp <lea.kampermann@gmail.com>
2015-04-14 17:03:51 +0200

    Outputting Log, option of p0 zero Trials

--- feargenET_PFfitting_YN.m
+++ feargenET_PFfitting_YN.m
@@ -1,10 +1,10 @@
-function [p]=feargenET_PFfitting_YN(subject,   csp_degree)
+function [p,Log]=feargenET_PFfitting_YN(subject,   csp_degree)
 
 % Diskrimination Task estimating the Threshold alpha and Slope beta of an
 % observer's underlying Psychometric Function (PF).
 % Enter the subject Number as well as the CS+ Face in Degrees (where 00 is
 % 1st face, and so)
-simulation_mode = 0;
+simulation_mode = 1;
 p = [];
 SetParams;
 SetPTB;
@@ -14,16 +14,16 @@ SetPTB;
 %Set up running fit procedure:
 
 %% Define prior, these are always the same so defining once is enough.
-prioraaRange    = linspace(0,180,50); %values of aa to include in prior
+prioraaRange    = linspace(0,100,50); %values of aa to include in prior
 %IS THIS RANGE OF BETA VALUES REASONABLE?
 priorBetaRange  = linspace(-2,0,50);  %values of log_10(beta) to include in prior
 %Range of lapse rates for the marginalized estimation of lambda
 %Prins(2013) uses 0:0.01:0.1;
-priorLambdaRange = 0:0.01:0.1;
+priorLambdaRange = 0:0.01:0.05;
 %Range of guess rates (Prins: 0:0.03:0.3);
 priorGammaRange = 0:0.03:0.3;
 % Stimulus values to select from (need not be equally spaced)
-stimRange       = 0:11.25:180;
+stimRange       = 0:11.25:100;
 
 
 % %2-D Gaussian prior
@@ -33,8 +33,13 @@ stimRange       = 0:11.25:180;
 
 
 %Termination after n Trials
-numtrials      = 2;
+numtrials0      = 60;
+% percentage of obligatory x=0 trials
+p0=.2;
+numtrials  = numtrials0+numtrials0*p0;
 
+% make a break every ....th Trial
+breakpoint=5;
 %Function to be fitted during procedure
 PFfit = @PAL_CumulativeNormal;    %Shape to be assumed
 
@@ -43,10 +48,11 @@ PM = [];
 face_shift   = [0 180 0 180];
 circle_shift = [0 0 360 360];
 circle_id    = [1 1 2 2]*p.stim.tFace/2;
-tchain = 4;
+tchain = 1;
 % counter for within chain trials (cc) and global trials (tt)
-cc=zeros(tchain,1);
+cc=zeros(1,tchain);
 tt=0; 
+
 for nc = 1:tchain
 %set up procedure
 PM{nc} = PAL_AMPM_setupPM('priorAlphaRange',prioraaRange,'priorBetaRange',...
@@ -57,22 +63,42 @@ PM{nc} = PAL_AMPM_setupPM('priorAlphaRange',prioraaRange,'priorBetaRange',...
 % 
 PM{nc}.reference_face   = face_shift(nc);
 PM{nc}.reference_circle = circle_shift(nc);
-
+if p0 ~= 0
+%     zerotrials(:,nc)=randsample([1:numtrials],numtrials0*p0);
+zerotrials(:,nc)=[1 3 5 7 9 11 13 15 17];
+end
 % set up Log Variable
 SetupLog(nc);
 end
 
-ShowInstruction(1);
+ShowInstruction(3);
 
-
-while (PM{1}.stop ~= 1) || (PM{2}.stop ~= 1) || (PM{3}.stop ~= 1) || (PM{4}.stop ~= 1)
-
+OK = 1;
+while OK
+    
     current_chain = randsample(1:tchain,1);
-  
-   
+    
+    
     if PM{current_chain}.stop ~= 1
         tt=tt+1;
+        % enter in Break Loop
+            if (tt~=1 && mod(tt,breakpoint)==1 && simulation_mode==0);
+                ShowInstruction(4);
+            end
         cc(current_chain)=cc(current_chain)+1;
+        fprintf('Chain %4.2f , Trial %02d\n',current_chain,cc)
+        fprintf('Original PM.x: %4.2f \n',PM{current_chain}.x(cc))
+        % manually force x=0 trial, if it fits the zerotrial condition
+        if p0 ~= 0
+           if any(zerotrials(:,current_chain)==cc(current_chain))
+             fprintf('Forcing x=0 Trial...\n')
+             PM{current_chain}.xCurrent=0;
+             PM{current_chain}.x(cc)=0;
+            
+           end
+        end
+        fprintf('PM.x is now %4.2f ',PM{current_chain}.x(cc))
+       
         %Present trial here at stimulus intensity PM.xCurrent and collect
         %response
         direction = randsample([-1 1],1);
@@ -93,7 +119,7 @@ while (PM{1}.stop ~= 1) || (PM{2}.stop ~= 1) || (PM{3}.stop ~= 1) || (PM{4}.stop
         ref       = mod(ref,360)+ PM{current_chain}.reference_circle;
         
 %         fprintf('Chain: %03d\nxCurrent: %6.2f\nDirection:%6.2f\n %6.2f -> %6.2f vs. %6.2f\n',current_chain,PM{current_chain}.xCurrent,direction,dummy,test,ref);
-        % start Trial
+%        start Trial
         fprintf('Starting Trial %03d/%03d.\n',tt,tchain*numtrials)
         
         [test_face, ref_face, signal] = Trial_YN(ref,test,circle_id(current_chain));
@@ -103,34 +129,35 @@ while (PM{1}.stop ~= 1) || (PM{2}.stop ~= 1) || (PM{3}.stop ~= 1) || (PM{4}.stop
         message1 = 'Waren die Gesichter unterschiedlich oder gleich?\n';
         message2 = 'Bewege den "Zeiger" mit der rechten und linken Pfeiltaste\n und bestätige deine Einschätzung mit der oberen Pfeiltaste.';
         if ~simulation_mode
-            [response_subj]      = RatingSlider(p.ptb.rect,2,Shuffle(1:2,1),p.keys.increase,p.keys.decrease,p.keys.confirm,{ 'Unterschiedlich' 'Gleich'},message1,message2,0);
+            [response_subj]      = RatingSlider(p.ptb.rect,2,Shuffle(1:2,1),p.keys.increase,p.keys.decrease,p.keys.confirm,{ 'unterschiedlich' 'gleich'},message1,message2,0);
             
-            %see if subject found the different pair of faces...
+            % see if subject found the different pair of faces...
             % buttonpress left (Yes) is response_subj=2, right alternative (No) outputs a 1.
             if (response_subj == 2 && signal == 1)
                 response=1;
-                fprintf('...Hit. \n')
+%                 fprintf('...Hit. \n')
             elseif (response_subj==1 && signal == 1)
                 response=0;
-                fprintf('...Miss. \n')
+%                 fprintf('...Miss. \n')
             elseif (response_subj == 2 && signal==0)
                 response=1;
-                fprintf('...False Alarm. \n')
+%                 fprintf('...False Alarm. \n')
             elseif (response_subj == 1 && signal == 0)
                 response=0;
-                fprintf('...Correct Rejection. \n')
+%                 fprintf('...Correct Rejection. \n')
             else
-                fprintf('error in the answer algorithm! \n')
+%                 fprintf('error in the answer algorithm! \n')
             end
             
         else
-            TrueThreshold   = 33;
-            Noise           = 5;
-            if PM{current_chain}.xCurrent > (TrueThreshold+randn(1)*Noise);
-                response = 1;
-            else
-                response = 0;
-            end
+            true_a = 45;
+            true_s = 30;
+            true_g = 0.2;
+            true_l = 0.02;
+            response = ObserverResponseFunction(PFfit,true_a,1/true_s,true_g,true_l,PM{current_chain}.xCurrent);
+            
+                    
+                       
         end
         
   
@@ -144,7 +171,9 @@ while (PM{1}.stop ~= 1) || (PM{2}.stop ~= 1) || (PM{3}.stop ~= 1) || (PM{4}.stop
         %updating PM
         PM{current_chain} = PAL_AMPM_updatePM(PM{current_chain},response);
         SetLog;
-    
+        %iteration control
+        dummy = cell2mat(PM);
+        OK    = sum([dummy(:).stop]) ~= tchain;
     end
     
 
@@ -161,55 +190,15 @@ for chain=1:tchain
 fprintf('Chain %g: Estimated Threshold (alpha): %4.2f \n',chain,PM{chain}.threshold(end));
 fprintf('Chain %g: Estimated Slope (beta): %4.2f \n',chain,PM{chain}.slope(end));
 end
-% PlotProcedure;
+% save PF Fit Plot
+feargenET_PFfitting_Fitplot(num2str(subject),Log)
 %clear the screen
 %close everything down
 cleanup;
 %move the folder to appropriate location
 movefile(p.path.subject,p.path.finalsubject);
 
-    function SetupLog(nc)
-        
-       
-        Log.globaltrial= NaN(nc,numtrials);
-        Log.signal     = NaN(nc,numtrials);
-        Log.x          = NaN(nc,numtrials);
-        Log.refface    = NaN(nc,numtrials);
-        Log.testface   = NaN(nc,numtrials);
-      
-        Log.response   = NaN(nc,numtrials);
-        Log.alpha      = NaN(nc,numtrials);
-        Log.seAlpha    = NaN(nc,numtrials);
-        Log.beta       = NaN(nc,numtrials);
-        Log.seBeta     = NaN(nc,numtrials);
-        Log.gamma      = NaN(nc,numtrials);
-        Log.seGamma    = NaN(nc,numtrials);
-        Log.lambda     = NaN(nc,numtrials);
-        Log.seLambda   = NaN(nc,numtrials);
-        Log.xrounded   = NaN(p.stim.tFace/tchain+1,numtrials,nc);
-        Log.trial_counter  = zeros(p.stim.tFace/tchain+1,nc);
-        
-    end
-
-    function SetLog
-        
-       
-        Log.globaltrial(current_chain,cc(current_chain))= tt;
-        Log.signal(current_chain,cc(current_chain))     = signal;
-        Log.x(current_chain,cc(current_chain))          = PM{current_chain}.xCurrent*direction;
-        Log.refface(current_chain,cc(current_chain))    = ref_face;
-        Log.testface(current_chain,cc(current_chain))   = test_face;
-        Log.response(current_chain,cc(current_chain))   = response;
-        Log.alpha(current_chain,cc(current_chain))      = PM{current_chain}.threshold(end);
-        Log.seAlpha(current_chain,cc(current_chain))    = PM{current_chain}.seThreshold(end);
-        Log.beta(current_chain,cc(current_chain))       = PM{current_chain}.slope(end);
-        Log.seBeta(current_chain,cc(current_chain))     = PM{current_chain}.seSlope(end);
-        Log.gamma(current_chain,cc(current_chain))      = PM{current_chain}.guess(end);
-        Log.seGamma(current_chain,cc(current_chain))    = PM{current_chain}.seGuess(end);
-        Log.lambda(current_chain,cc(current_chain))     = PM{current_chain}.lapse(end);
-        Log.seLambda(current_chain,cc(current_chain))   = PM{current_chain}.seLapse(end);
-        
-    end
+    
 
     function  [test_face, ref_face, signal] = Trial_YN(ref_stim,test_stim,last_face_of_circle)
         %         computes the trial FACES, using the test/ref information input
@@ -236,9 +225,9 @@ movefile(p.path.subject,p.path.finalsubject);
         sprite_index = [100 trial(1) NaN 100 trial(2) NaN];
         faces_trial = trial;
         
-        fprintf('...Chain: %02d \n',current_chain)
-        fprintf('...xDelta: %4.2f Degrees.\n',PM{current_chain}.xCurrent*direction)
-        fprintf('...Faces Trial: '),fprintf('%02d %02d\n',faces_trial(1),faces_trial(2))
+%         fprintf('...Chain: %02d \n',current_chain)
+%         fprintf('...xDelta: %4.2f Degrees.\n',PM{current_chain}.xCurrent*direction)
+%         fprintf('...Faces Trial: '),fprintf('%02d %02d\n',faces_trial(1),faces_trial(2))
         onsets     = p.trial.onsets + GetSecs;
         
         for i = 1:length(sprite_index)
@@ -262,7 +251,7 @@ movefile(p.path.subject,p.path.finalsubject);
         %         Screen('Flip',p.ptb.w,TimeStimOnset,0)
     end
 
-  function SetPTB
+    function SetPTB
     debug =0;
         %Open a graphics window using PTB
         screens       =  Screen('Screens');
@@ -306,7 +295,7 @@ movefile(p.path.subject,p.path.finalsubject);
         end
         p.stim.delta = 720/p.stim.tFile;
     end
-  function SetParams
+    function SetParams
     
         %
         p.var.timings                 = zeros(1,10);
@@ -398,13 +387,14 @@ movefile(p.path.subject,p.path.finalsubject);
         %these are the intervals of importance
         %time2fixationcross->cross2onset->onset2shock->shock2offset
         %these (duration.BLA) are average duration values:
-        p.duration.stim                = 1.5;%s     
-        p.duration.pink                = .5;
-        p.duration.gray                = .5;
+%         1.5 0.5 0.5
+        p.duration.stim                = 0.35;%s     
+        p.duration.pink                = .2;
+        p.duration.gray                = .2;
         if simulation_mode
-            p.duration.stim                = .01;%s
-            p.duration.pink                = .01;
-            p.duration.gray                = .01;
+            p.duration.stim                = .001;%s
+            p.duration.pink                = .001;
+            p.duration.gray                = .001;
         end
         %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
         
@@ -430,7 +420,7 @@ movefile(p.path.subject,p.path.finalsubject);
         %
 
   end
-  function [rating]=RatingSlider(rect,tSection,position,up,down,confirm,labels,message1,message2,numbersOn)
+    function [rating]=RatingSlider(rect,tSection,position,up,down,confirm,labels,message1,message2,numbersOn)
         %
         %Detect the bounding boxes of the labels.
         for nlab = 1:2
@@ -488,20 +478,20 @@ movefile(p.path.subject,p.path.finalsubject);
             Screen('TextSize', p.ptb.w,p.text.fontsize);
             DrawFormattedText(p.ptb.w,message1, 'center', p.ptb.midpoint(2)*0.2,  p.stim.white,[],[],[],2);
             Screen('TextSize', p.ptb.w,p.text.fontsize./2);
-             DrawFormattedText(p.ptb.w,message2, 'center', p.ptb.midpoint(2)*0.4,  p.stim.white,[],[],[],2);
+            DrawFormattedText(p.ptb.w,message2, 'center', p.ptb.midpoint(2)*0.4,  p.stim.white,[],[],[],2);
             Screen('TextSize', p.ptb.w,p.text.fontsize);
             Screen('Flip',p.ptb.w);
         end
-  end
+    end
 
-  function shuffled = Shuffle(vector,N)
+    function shuffled = Shuffle(vector,N)
         %takes first N from the SHUFFLED before outputting. This function
         %could be used as a replacement for randsample
         if nargin < 2;N = length(vector);end
         [dummy, idx]    = sort(rand([1 length(vector)]));
         shuffled        = vector(idx(1:N));
         shuffled        = shuffled(:);
-  end
+    end
 
     function ShowInstruction(nInstruct)
         
@@ -514,42 +504,100 @@ movefile(p.path.subject,p.path.finalsubject);
         Screen('FillRect',p.ptb.w,p.stim.bg);
         Screen('Flip',p.ptb.w);
         
-        function ShowText(text)
-            
-            
-            Screen('FillRect',p.ptb.w,p.stim.bg);
-            %DrawFormattedText(p.ptb.w, text, p.text.start_x, 'center',p.stim.white,[],[],[],2,[]);
-            DrawFormattedText(p.ptb.w, text, 'center', 'center',p.stim.white,[],[],[],2,[]);
-            Screen('Flip',p.ptb.w);
-            
-            %show the messages at the experimenter screen
-            fprintf([repmat('=',1,50) '\n']);
-            fprintf('Subject''s monitor:\n');
-            fprintf(text);
-            fprintf([repmat('=',1,50) '\n']);
-            
-        end
+    end
+    function ShowText(text)
         
-        function [text]=GetText(nInstruct);
+        
+        Screen('FillRect',p.ptb.w,p.stim.bg);
+        %DrawFormattedText(p.ptb.w, text, p.text.start_x, 'center',p.stim.white,[],[],[],2,[]);
+        DrawFormattedText(p.ptb.w, text, 'center', 'center',p.stim.white,[],[],[],2,[]);
+        Screen('Flip',p.ptb.w);
+        
+        %show the messages at the experimenter screen
+        fprintf([repmat('=',1,50) '\n']);
+        fprintf('Subject''s monitor:\n');
+        fprintf(text);
+        fprintf([repmat('=',1,50) '\n']);
+        
+    end
+
+
+    function [text]=GetText(nInstruct);
+        
+        
+        if nInstruct == 1
+            text = ['Du siehst nun nacheinander zwei Gesichter.\n'...
+                '\n'...
+                'Danach wirst Du gefragt, ob die Gesichter unterschiedlich oder gleich waren.\n'...
+                '\n'...
+                'Benutze dazu die Pfeiltasten (links, rechts) und die obere Taste zum Bestätigen.\n'...
+                '\n'...
+                'Wenn du noch Fragen hast, kannst du jetzt den Versuchsleiter fragen.\n'...
+                '\n'...
+                'Drücke ansonsten die mittlere Taste,\n'...
+                '   um das Experiment zu starten.\n' ...
+                ];
+        elseif nInstruct == 3
+            text = ['You will now see two faces after each other.\n'...
+                '\n'...
+                'Please state, if they were different (left) or the same (right).\n'...
+                '\n'...
+                'Use the arrow keys to move the marker and confirm with the UP button.\n'...
+                '\n'...
+                'Please press UP\n'...
+                '   to start the experiment!\n' ...
+                ];
+        elseif nInstruct == 2%end
+            text = ['Experiment beendet!\n'];
             
-            
-            if nInstruct == 1
-                text = ['Du siehst nun nacheinander zwei Gesichter.\n'...
-                    '\n'...
-                    'Danach wirst Du gefragt, ob die Gesichter unterschiedlich oder gleich waren.\n'...
-                    '\n'...
-                    'Benutze dazu die Pfeiltasten (links, rechts) und die obere Taste zum Bestätigen.\n'...
-                    '\n'...
-                    'Wenn du noch Fragen hast, kannst du jetzt den Versuchsleiter fragen.\n'...
-                    '\n'...
-                    'Drücke ansonsten die mittlere Taste,\n'...
-                    '   um das Experiment zu starten.\n' ...
-                    ];
-            elseif nInstruct == 2%end
-                text = ['Experiment beendet!\n'];
-                
-            end
+        elseif nInstruct==4%break
+            text = [sprintf('Du hast bereits %g von %g Durchgängen geschafft!\n',tt-1,numtrials*tchain)...
+                'Mache eine kurze Pause.\n'...
+                'Drücke anschließend die mittlere Taste, um weiterzumachen.\n'];
         end
+    end
+       
+    function SetupLog(nc)
+        
+        
+        Log.globaltrial= NaN(nc,numtrials);
+        Log.signal     = NaN(nc,numtrials);
+        Log.x          = NaN(nc,numtrials);
+        Log.refface    = NaN(nc,numtrials);
+        Log.testface   = NaN(nc,numtrials);
+        
+        Log.response   = NaN(nc,numtrials);
+        Log.alpha      = NaN(nc,numtrials);
+        Log.seAlpha    = NaN(nc,numtrials);
+        Log.beta       = NaN(nc,numtrials);
+        Log.seBeta     = NaN(nc,numtrials);
+        Log.gamma      = NaN(nc,numtrials);
+        Log.seGamma    = NaN(nc,numtrials);
+        Log.lambda     = NaN(nc,numtrials);
+        Log.seLambda   = NaN(nc,numtrials);
+        Log.xrounded   = NaN(p.stim.tFace/tchain+1,numtrials,nc);
+        Log.trial_counter  = zeros(p.stim.tFace/tchain+1,nc);
+        
+    end
+
+    function SetLog
+        
+        
+        Log.globaltrial(current_chain,cc(current_chain))= tt;
+        Log.signal(current_chain,cc(current_chain))     = signal;
+        Log.x(current_chain,cc(current_chain))          = PM{current_chain}.x(cc(current_chain))*direction;
+        Log.refface(current_chain,cc(current_chain))    = ref_face;
+        Log.testface(current_chain,cc(current_chain))   = test_face;
+        Log.response(current_chain,cc(current_chain))   = response;
+        Log.alpha(current_chain,cc(current_chain))      = PM{current_chain}.threshold(end);
+        Log.seAlpha(current_chain,cc(current_chain))    = PM{current_chain}.seThreshold(end);
+        Log.beta(current_chain,cc(current_chain))       = PM{current_chain}.slope(end);
+        Log.seBeta(current_chain,cc(current_chain))     = PM{current_chain}.seSlope(end);
+        Log.gamma(current_chain,cc(current_chain))      = PM{current_chain}.guess(end);
+        Log.seGamma(current_chain,cc(current_chain))    = PM{current_chain}.seGuess(end);
+        Log.lambda(current_chain,cc(current_chain))     = PM{current_chain}.lapse(end);
+        Log.seLambda(current_chain,cc(current_chain))   = PM{current_chain}.seLapse(end);
+        
     end
 %     function PlotProcedure
 %         plotproc=figure(1);
