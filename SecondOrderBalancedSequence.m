07d3b0b2049eeaef8f5f9cc6d1d735e3dae2c704
onatselim <onatselim@gmail.com>
2015-04-16 22:31:57 +0200

    Adapted to any arbitrarily shaped transition matrices.

--- SecondOrderBalancedSequence.m
+++ SecondOrderBalancedSequence.m
@@ -2,7 +2,7 @@ function [S,i]=SecondOrderBalancedSequence(tItem,repetition,start_condition)
 %[S,i]=SecondOrderBalancedSequence(tItem,repetition,start_condition)
 %
 %   Generates a so-called t1r1 sequence, see Aguirre for fMRI applications.
-%   These sequences are balanced over second-order transitions. 
+%   These sequences are balanced over second-order transitions.
 %
 %   TITEM is the total number of conditions. REPETITION is the number of
 %   transitions. If REPETITION is equal to 1, all transitions are repeated
@@ -13,7 +13,7 @@ function [S,i]=SecondOrderBalancedSequence(tItem,repetition,start_condition)
 %   transitions possible. START_CONDITION indicates the starting condition
 %   of the sequence.
 %
-%   Example Usage: 
+%   Example Usage:
 %   [S,i]=SecondOrderBalancedSequence(8,ones(1,8)*34,1)
 %
 %   Sonja Schall, Selim Onat, 01-May-2012 19:34:05
@@ -23,50 +23,53 @@ function [S,i]=SecondOrderBalancedSequence(tItem,repetition,start_condition)
 %   on the sequence as a matrix I. I(1,2,:) gives the locations of 1 to 2
 %   transitions on the sequence S.
 
-% fprintf('Creating 2nd order balanced sequence...\n')
-counter = 0;
-success = 0;
-while ~success%when T sums to 0
-    
-    %T=ones(tItem,tItem) .* repetition;%the old way, equal number of trials
-    %for each condition...
-    %new way:
+%%
+%T=ones(tItem,tItem) .* repetition;%the old way, equal number of trials
+%for each condition...
+%new way:
+if isvector(repetition);
     c = 0;
     T = [];
     repetition = repetition(:)';
     for x_ = repetition(end:-1:1)
         c  = c+1;
         T(1:length(repetition)-c+1,1:length(repetition)-c+1) = x_;
-    end
-    Tori = T;
-    %%check whether the count matrix is valid.
+    end        
+else
+    fprintf('Matrix given, will treat the 2nd argument as count matrix.\n');
+    T = repetition;    
+end
+%% check whether the count matrix is valid.
     if sum(sum(abs(T - round(T)) == 0)) ~= length(T(:))
-        T
         S=[];
         fprintf('The repetition vector should consists of integers\n');
-        return       
+        return
     end
+Tori = T;
+%%
+success = 0;
+while ~success%when T sums to 0
     %
+    T   = Tori;
     row = ceil(start_condition);%the first stimulus.
-    col = ceil(rand*tItem);%the second stimulus.
+    col = randsample(find(T(row,:)),1);%ceil(rand*tItem);%the second stimulus.
     S   = [row col];
     ok  = 1;
     counter = 0;
     while ok%it is OK when next stim is correctly selected
         %store the location of the transition on the sequence
         counter = counter + 1;
-        i(row,col,T(row,col)) = counter; 
+        i(row,col,T(row,col)) = counter;
         
         %decrement this value
         T(row,col)=T(row,col)-1;
         
         row=col;%second stimulus becomes the first
-        col=[];%we have to select the new second one now
-        
+        col=[];%we have to select the new second one now        
         col_pool=find(T(row,:));
         if isempty(col_pool)%no more transition possible
             ok=0;
-        else            
+        else
             %pick one of the possible transitions
             %This is the old way, this generates problems when one of the
             %conditions has significantly less repetitions, it results in
@@ -78,7 +81,7 @@ while ~success%when T sums to 0
             %T(row,col_pool) represents the probabilities
             next_stim = SyntFix_Core(Tori(row,col_pool),1);
             col       = col_pool(next_stim);
-            S         = [S col];            
+            S         = [S col];
         end
         
     end
@@ -87,9 +90,9 @@ while ~success%when T sums to 0
         success=1;
         %remove the zeros due to unequal transition numbers
         i(i == 0) =NaN;
-    end   
+    end
 end
-% % 
+% %
 % % fprintf('Checking balancing...\n')
 % % [n c] = hist3([S(1:end-1) ;S(2:end)]',{1:tItem 1:tItem});
 % % imagesc(n);colorbar;drawnow;
@@ -99,14 +102,14 @@ end
 % %     keyboard
 % %   %  S = [];
 % % else
-% %     fprintf('Sequence is balanced...\n')  
+% %     fprintf('Sequence is balanced...\n')
 % % end
 % % fprintf('first order histogram:\n')
 % % mat2str(co)
 % % fprintf('second order balancing:\n');
 % % display(n)
 
-function [i] = SyntFix_Core(saliency,tfix);
+    function [i] = SyntFix_Core(saliency,tfix);
         %[i] = SyntFix_Core(s,tfix);
         %
         %Given a matrix SALIENCY representing a topographic saliency, this function
@@ -121,7 +124,7 @@ function [i] = SyntFix_Core(saliency,tfix);
         %fixation init
         i   = zeros(tfix,1);
         %%!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-        %%the reason for 0.66: it makes the (oddball + ucs) probabilities 
+        %%the reason for 0.66: it makes the (oddball + ucs) probabilities
         %across time as flat as possible over time;
         %!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
         s   = cumsum(saliency(:).^(0.66));
@@ -142,5 +145,5 @@ function [i] = SyntFix_Core(saliency,tfix);
             %we will take the last negative or zero entry in DUMMY; The index of
             %this entry will be the location that is fixated...
         end
-end
+    end
 end
