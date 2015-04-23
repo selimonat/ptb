48ec537c77defa4fbfbd7581c3c5ae207f3208e4
onatselim <onatselim@gmail.com>
2015-04-23 09:32:11 +0200

    branch cleaning

--- V1Model2MDS.m
+++
@@ -1,7 +0,0 @@
-function [mds]=V1Model2MDS(ed,mds_dim)
-%[mds]=V1Model2MDS(ed,mds_dim)
-%
-% Runs mds analysis with MDS_DIM dimensions on euclidian distances.
-
-
-mds   = mdscale(ed,mds_dim); %set to (im,2) for 2-dimensional scaling.
