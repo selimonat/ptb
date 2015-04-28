795e4c59d11f6f0240683403cab44adf7565ee52
leakp <lea.kampermann@gmail.com>
2015-04-28 19:15:54 +0200

    work in progress, new sanity check structure

--- seq_feargen_eyelab.m
+++ seq_feargen_eyelab.m
@@ -1,18 +1,16 @@
 function [seq]=seq_feargen_eyelab(condition,csp)
 % FEARGENSEQUENCE_ET
 % for 1 Circle!
-%
-% creates a sequence for all the gives faces (as defined by numbers in
-% input FACE_STIMULI), adding UCS and Oddball trials as defined by the
-% rates pOddball and pReinforcement.
-% CSP (in face Number) contains the CSplus face.
-% REP is the number of how often every transition (between faces) should be repeated (used by the
-% SecondOrderBalancedSequence Script).
-% method =1 for Baseline + Test, method=2 for Conditioning
 
-% Reinforcement Rate (in Baseline + Test its automatically added, but in
-% Cond. it needs to take RR of all the CSP faces as ucs.
-seq.RR             = 0.3;
+% 
+%
+% creates a sequence for all the given faces, plus ucs and oddball.
+% The sequence is 2ndOrderBalanced regarding Face Transitions, ISI and
+% Fixation Crosses (balanced only left/right, not real angle).
+% CSP (in face Number) contains the CSplus face.
+% 
+% condition='b' for Baseline, 't' for Test, and 'c' for Conditioning.
+
 trialduration      = .75;
 % minimum ISI
 mini_isi       = 1.5;
@@ -34,38 +32,29 @@ if strcmp(condition,'c')
     stimuli = [csp csn csp oddball];%later oddball will be assigned randomly
     valid_oddballs         = mod([csp csn]+2,8);
     tface = 2;
-elseif strcmp(condition,'t') ||  strcmp(condition,'b')
+elseif strcmp(condition,'t') || strcmp(condition,'b')
     rep_vec = [ones(1,8)*4 1 1];
     stimuli = [1:8 csp oddball];%ucs's are taken from the CSP indices
     valid_oddballs         = mod([csp csn]+2,8);
     tface = 8;
+elseif strcmp(condition,'b')
+    % stimuli has to include stim_id=ucs==9, to keep the shock signal
+    stimuli = [1:8 ucs oddball];
 end
 
 
 
 %% create the block-wise structure of trials
-nb = 1;
+
 seq.cond_id = [];
 seq.CrossPosition=[];
 OK=0;
 seq.ucs   = logical(zeros(1,length(seq.cond_id)));
 while ~OK
     
-    % % if you need the chess fieldish structure of 1 and 2 to extend some
-    % % transitions' repetitions:
-    %     m=ones(length(stimuli),length(stimuli));
-    %     m(1:2:end-2,2:2:end-2)=2;
-    %     m(2:2:end-2,1:2:end-2)=2;
-    
-    
-    %% Add the ucs trials, here we have to choose either by keeping the CSP
-    %% faces seen by the subjects constant or not
-    
-    % this method 1 doesn't keep the amount of CSP faces (shocked +
-    % non-schocked) equal to the repetition of other stimuli. so overall
-    % participants see the CSP face a little bit more.
-    
-    [seq.cond_id,i]= seq_SecondOrderBalancedSequence(rep_vec,1);
+    %create the 2nd Order Balanced Sequence
+    [seq.cond_id]= seq_SecondOrderBalancedSequence(rep_vec,1);
+    %replace Conditions with real Stimuli numbers
     seq.stim_id    = stimuli(seq.cond_id);
     
     seq.oddball              = seq.cond_id == tface+2;
@@ -73,9 +62,21 @@ while ~OK
     
     seq.ucs     = seq.cond_id == (tface + 1);
     tucs = sum(seq.ucs);
-    seq.tTrial             = length(seq.cond_id)
+    seq.tTrial             = length(seq.cond_id);
+
     
-    OK=1%sanity_check;
+    
+% % % % %     
+    fprintf('Starting Sanity Check....\n')
+    if ExcludeEvents(seq.ucs,0.95) || ExcludeEvents(seq.oddball,0.95) || ExcludeEvents(seq.oddball,0.1)|| SlopeCheck(seq.ucs)  || LongestNoEventDistance(seq.ucs,40) || TucsTest(seq.ucs,tucs)
+    
+        OK=0;
+        fprintf('...creating New Sequence.\n')
+    else
+        OK=1;
+        fprintf('Sanity Check: OK. \n')
+    end
+        
 end
 
 %% final variables
@@ -108,15 +109,8 @@ elseif strcmp(balancing,'quasiuniform')
         
         for i = unique(seq.cond_id(nexts_clean))
             %for each destination face, randomly assign an ISI
-%             seq.isi() = randsample(isis,rep);
             pos = nexts_clean(seq.cond_id(nexts_clean) == i)-1;
-%             
             seq.isi(pos) = seq_BalancedDist(ones(1,length(pos)),isis);
-%             
-%             isipooldummy=Shuffle(repmat(isis,[1
-%             ceil(rep/length(isis))+1]));
-%             seq.isi(nexts_clean(seq.cond_id(nexts_clean) == i)-1) = isipooldummy(1:length(seq.isi(nexts_clean(seq.cond_id(nexts_clean) == i)-1)));
-%             
         end        
     end
     %find transitions FROM ucs/odd, set them to max(isi)
@@ -126,38 +120,12 @@ elseif strcmp(balancing,'quasiuniform')
     nanpos=isnan(seq.isi);
     seq.isi(isnan(seq.isi))=seq_BalancedDist(ones(1,sum(nanpos)),isis);
     seq.cond_id(end)=[];
-    % % % % %         elseif method ==2
-    % % % % %             % needs another balancing, because UCS is not an own balanced
-    % % % % %             % condition, but within the CSP faces.
-    % % % % %             %first find UCS and Oddballs and set them to max(isi)
-    % % % % %             events_i=seq.ucs|seq.oddball;
-    % % % % %             seq.isi(events_i)=max(isis);
-    % % % % %             %then take no-ucs CSP faces and CSN faces and assign ISIs
-    % % % % %             %randomly
-    % % % % %             for first = 1:length(face_stimuli);
-    % % % % %                 first_i=find(seq.cond_id==first&isnan([seq.isi NaN]));
-    % % % % %                 nexts   = first_i+1;%transition to the following face
-    % % % % %                 %nexts_clean = nexts(seq.cond_id(nexts) < length(face_stimuli)+1); %exclude trans to ucs/odd
-    % % % % %                 nexts_clean = nexts(~(seq.ucs|seq.oddball));
-    % % % % %
-    % % % % %                 for i = find(unique(seq.cond_id(nexts_clean))<max(face_stimuli));
-    % % % % %                     %for each destination face i, randomly assign an ISI
-    % % % % %                     %create vector containing enough ISI material
-    % % % % %                     seq.isi(nexts_clean(seq.cond_id(nexts_clean) == i)-1) = seq_BalancedDist(seq.isi(nexts_clean(seq.cond_id(nexts_clean) == i)-1),isis);
-    % % % % %                 end
-    % % % % %             end
-    % % % % %
-    % % % % %             % from faces TO ucs/odd should also be nicely random
-    % % % % %
-    % % % % %             seq.isi(isnan(seq.isi))= seq_BalancedDist(seq.isi(isnan(seq.isi)),isis);
-    % % % % %             seq.isi(end)=7;
-    % % % % %             seq.cond_id(end)=[];
-    %         end
+    
     
 end
 
-% duration = sum(seq.isi)+length(seq.cond_id)*trialduration;
-% fprintf('Total duration is %02g minutes\n',duration./60);
+duration = sum(seq.isi)+seq.tTrial*trialduration;
+fprintf('Total duration is %02g minutes.\n',duration./60);
 
 FixationCrossSequence;
 
@@ -171,63 +139,56 @@ if visualization == 1
     hold off;
 end
 
-    function [OK]=sanity_check
-        OK=0;
-        % if ~isempty(oddball) && ~isempty(ucs)
-        % % check if number of UCS is correct
-        if sum(seq.ucs)==tucs
-            % check for time trends in UCS
-            rate=conv(double(seq.ucs),ones(1,20),'valid')';
-            X = [[1:length(rate)]' ones(length(rate),1)];
-            [b bint]= regress( rate, X );
-            
-            if bint(1)<=0 && bint(1,2)>=0
-                %then 0 is within the CI, so it's not significant
-                fprintf('No time trend in UCS.\n')
-                if method==1
-                    rate=conv(double(seq.oddball),ones(1,20),'valid')';
-                    X = [[1:length(rate)]' ones(length(rate),1)];
-                    [b bint]= regress( rate, X );
-                    if bint(1)<=0 && bint(1,2)>=0
-                        fprintf('No time trend in Oddballs.\n')
-                        OK=1;
-                    else
-                        fprintf('Time trend in Oddballs...Creating New Sequence..\n')
-                        OK=0;
-                    end
-                elseif method==2
-                    % skip the Oddball time trend analyzer here... too few
-                    % oddballs anyway
-                    OK=1;
-                end
-            else
-                OK=0;
-                fprintf('Time trend in UCS...Creating New Sequence.\n')
-            end
-        else
-            fprintf('%d UCS found instead of %d!\n',sum(seq.ucs),tucs)
-            OK=0;
-        end
-    end
 
     function FixationCrossSequence
         seq.CrossPosition = nan(1,seq.tTrial);
-%         cross_angles=linspace(0,30,17);
+        
         radius=520; %in px
-        cross_direction=[0 180];
-        center         = [800 600]
+        cross_direction = [0 180];
+        center          = [800 600];
         for ncond = 1:tface
             ind                              = seq.cond_id == ncond;
-            seq.CrossPosition(ind) = seq_BalancedDist(ones(1,sum(ind)),cross_direction);
+            seq.CrossPosition(ind)           = seq_BalancedDist(ones(1,sum(ind)),cross_direction);
         end
         
-        seq.CrossPosition(isnan(seq.CrossPosition)) = seq_BalancedDist(ones(1,sum(isnan(seq.CrossPosition))),cross_direction);     
+        %Transitions to/from UCS/Oddball.
+        seq.CrossPosition(isnan(seq.CrossPosition)) = seq_BalancedDist(ones(1,sum(isnan(seq.CrossPosition))),cross_direction);
         
-        seq.CrossPosition = seq.CrossPosition + rand(1,293)*30-15;
+        seq.CrossPosition = seq.CrossPosition + rand(1,length(seq.cond_id))*30-15;
         seq.CrossPosition=[cosd(seq.CrossPosition')*radius+center(1) sind(seq.CrossPosition')*radius+center(2)];
         
     end
 
 
+
+    function [out]= TucsTest(seq,reference)
+        %check if number of UCS is tUCS as supposed
+        out=sum(seq)~=reference;
+    end
+
+    function [out]=ExcludeEvents(seq,part)
+        %returns TRUE if there is no event on the last percentage of the
+        %sequence.
+        out = sum(find(seq)>= part*length(seq)) ==0;
+    end
+
+
+    function [out]=LongestNoEventDistance(seq,distance)
+        %the longest distance where nothing happens.
+        out = max(sort(diff(find(seq)))) < distance;
+    end
+
+    function [out]=SlopeCheck(seq)
+        
+        rate     = conv(double(seq),ones(1,30),'valid');
+        X        = [(1:length(rate))' ones(length(rate),1)];
+        [b bint] = regress( rate(:), X );
+        if bint(1) <=0 && bint(1,2)>=0
+            out = false;
+        else
+            out=true;
+        end
+        
+    end
 
 end
