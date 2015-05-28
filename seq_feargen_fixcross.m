5e5dfe4d1af25d8cb5bdb5e6bd61bd282dad770e
leakp <lea.kampermann@gmail.com>
2015-05-28 19:35:14 +0200

    corrected Output structure

--- seq_feargen_fixcross.m
+++ seq_feargen_fixcross.m
@@ -1,8 +1,8 @@
-function [s] = seq_feargen_fixcross(s)
+function [CrossPosition] = seq_feargen_fixcross(s)
 %SEQ_FEARGEN_FIXCROSS
 %sets up all the fixation cross positions left/right from the face
 
-s.CrossPosition = nan(1,s.tTrial);
+CrossPosition = nan(1,s.tTrial);
 tface           = length(unique(s.cond_id) > 0)-2;
 
 radius=590; %in px
@@ -10,14 +10,14 @@ cross_direction = [0 180];
 center          = [800 600];
 for ncond = 1:tface
     ind                            = s.cond_id == ncond;
-    s.CrossPosition(ind)           = seq_BalancedDist(ones(1,sum(ind)),cross_direction);
+    CrossPosition(ind)           = seq_BalancedDist(ones(1,sum(ind)),cross_direction);
 end
 
 %Transitions to/from UCS/Oddball.
-s.CrossPosition(isnan(s.CrossPosition)) = seq_BalancedDist(ones(1,sum(isnan(s.CrossPosition))),cross_direction);
+CrossPosition(isnan(CrossPosition)) = seq_BalancedDist(ones(1,sum(isnan(CrossPosition))),cross_direction);
 
-s.CrossPosition = s.CrossPosition + rand(1,length(s.cond_id))*30-15;
-s.CrossPosition=round([cosd(s.CrossPosition')*radius+center(1) sind(s.CrossPosition')*radius+center(2)]);
+CrossPosition = CrossPosition + rand(1,length(s.cond_id))*30-15;
+CrossPosition=round([cosd(CrossPosition')*radius+center(1) sind(CrossPosition')*radius+center(2)]);
 
 
 
