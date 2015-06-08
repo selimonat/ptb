43074504c7e864bdd161f1026331294db6253da5
leakp <lea.kampermann@gmail.com>
2015-06-08 15:20:14 +0200

    FX cross closer to face (radius 420px&smaller angle)

--- seq_feargen_fixcross.m
+++ seq_feargen_fixcross.m
@@ -5,7 +5,7 @@ function [CrossPosition] = seq_feargen_fixcross(s)
 CrossPosition = nan(1,s.tTrial);
 tface           = length(unique(s.cond_id) > 0)-2;
 
-radius=590; %in px
+radius=420; %in px
 cross_direction = [0 180];
 center          = [800 600];
 for ncond = 1:tface
@@ -16,7 +16,7 @@ end
 %Transitions to/from UCS/Oddball.
 CrossPosition(isnan(CrossPosition)) = seq_BalancedDist(ones(1,sum(isnan(CrossPosition))),cross_direction);
 
-CrossPosition = CrossPosition + rand(1,length(s.cond_id))*30-15;
+CrossPosition = CrossPosition + rand(1,length(s.cond_id))*15-7.5;
 CrossPosition=round([cosd(CrossPosition')*radius+center(1) sind(CrossPosition')*radius+center(2)]);
 
 
