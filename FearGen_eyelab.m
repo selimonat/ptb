738c214111a5399eda3c07525d27a0b1fabfcf24
leakp <lea.kampermann@gmail.com>
2015-06-22 19:27:20 +0200

    back to loading sequences, bc of stimPC

--- FearGen_eyelab.m
+++ FearGen_eyelab.m
@@ -1,4 +1,4 @@
-function [p]=FearGen_eyelab(subject,phase,csp,PainThreshold)
+function [p]=FearGen_eyelab(subject,phase,csp,PainThreshold,nth)
 %[p]=Conditioning(subject,NthSeq,CSpface,phase,PainThreshold)
 %
 %Used for the last recording (3rd Scan Request) sessions of the Feargen
@@ -413,15 +413,15 @@ cleanup;
         p.duration.prestim             = .85;
         %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
          %stimulus sequence
-%          seqpool = load('C:\Users\onat\Documents\Experiments\feargen_master\seq\seq.mat');
-%          seq = seqpool.s(nth);
-        if phase == 2
-            seq = seq_feargen_cloudseq(csp,'baseline');%only 1 short seq
-        elseif phase == 3%conditioning
-            seq = seq_feargen_cloudseq(csp,'cond');%gets 1 short seq
-        elseif phase == 4
-            seq = seq_feargen_cloudseq(csp,'test');%gets 2 short seqs
-        end
+         seqpool = load('C:\Users\onat\Documents\Experiments\feargen_master\seq\seq.mat');
+         seq = seqpool.s(nth);
+%         if phase == 2
+%             seq = seq_feargen_cloudseq(csp,'baseline');%only 1 short seq
+%         elseif phase == 3%conditioning
+%             seq = seq_feargen_cloudseq(csp,'cond');%gets 1 short seq
+%         elseif phase == 4
+%             seq = seq_feargen_cloudseq(csp,'test');%gets 2 short seqs
+%         end
         %create the randomized design
         p.stim.cs_plus                 = csp;%index of cs stimulus, this is the one paired to shock
         p.stim.cs_neg                  = csn;
