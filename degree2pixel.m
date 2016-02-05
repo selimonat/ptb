154c602cc6b334830cdfa428089414df08026001
onatselim <onatselim@gmail.com>
2016-02-05 14:39:39 +0100

    final version with deg2pix and pix2deg conversion possibilities

--- degree2pixel.m
+++ degree2pixel.m
@@ -1,20 +1,35 @@
-function [pixel]=degree2pixel(where,deg)
+function [out]=degree2pixel(where,in,direction)
 %[pixel]=degree2pixel(where,deg)
 %
 %
-%   Will return approximate number of pixels that corresponds to a given
-%   degree of visual angle in the scanner or eyelab.
+%   Will return approximate number of pixels/degrees that corresponds to a given
+%   degree/pixels on the monitors used in the scanner or eyelab.
+%
+%   If DIRECTION is deg2pix, degrees are transformed to pixels, for the
+%   inverse transformation use pix2deg.
+%
+%   Example:
+%   degree2pixel('scanner',6,1) will return the number of pixels
+%   corresponding to 6 degrees in the scanner monitor.
 %
 %   Selim Onat.
 
-if strcmp(where,'scanner')
-    
-    distance2monitor = 92;%cm
-    left2right       = 31.4;%cm    
-    angular_span     = atand((left2right/2)./distance2monitor)*2;%pixels per degree    
-    pixelperdegree   = left2right/angular_span;    
-    pixel            = pixelperdegree*deg;
-    
-elseif strcmp(where,'eyelab')
-    
+if strcmp(where,'scanner')    
+    distance2monitor  = 92;%cm
+    left2right.metric = 31.4;%cm
+    left2right.pixel  = 1024;%native PLCXP200L resolution
+    angular_span      = atand((left2right.metric/2)./distance2monitor)*2;%pixels per degree
+    pixelperdegree    = left2right.pixel/angular_span;    
+elseif strcmp(where,'eyelab')    
+    distance2monitor  = 50;%cm
+    left2right.metric = 40.7;%cm
+    left2right.pixel  = 1600;%native PLCXP200L resolution
+    angular_span      = atand((left2right.metric/2)./distance2monitor)*2;%pixels per degree
+    pixelperdegree    = left2right.pixel/angular_span;
+end
+
+if strcmp(direction,'deg2pix'); %degree 2 pixels
+    out           = pixelperdegree*in;
+else strcmp(direction,'pix2deg'); %degree 2 pixels
+    out           = 1./pixelperdegree*in;
 end
