68f460a2d8e97015c523c66b336daa479555ae95
Selim Onat <onat@nin.uke.uni-hamburg.de>
2016-02-07 15:48:16 +0100

    physical measurements corrected

--- degree2pixel.m
+++ degree2pixel.m
@@ -1,5 +1,5 @@
 function [out]=degree2pixel(where,in,direction)
-%[pixel]=degree2pixel(where,deg)
+%[out]=degree2pixel(where,in,direction)
 %
 %
 %   Will return approximate number of pixels/degrees that corresponds to a given
@@ -8,6 +8,8 @@ function [out]=degree2pixel(where,in,direction)
 %   If DIRECTION is deg2pix, degrees are transformed to pixels, for the
 %   inverse transformation use pix2deg.
 %
+%	IN/OUT depending on the direction are in units of cm or pixels.
+%
 %   Example:
 %   degree2pixel('scanner',6,1) will return the number of pixels
 %   corresponding to 6 degrees in the scanner monitor.
@@ -15,8 +17,8 @@ function [out]=degree2pixel(where,in,direction)
 %   Selim Onat.
 
 if strcmp(where,'scanner')    
-    distance2monitor  = 92;%cm
-    left2right.metric = 31.4;%cm
+    distance2monitor  = 94.5;%cm
+    left2right.metric = 38.2;%cm
     left2right.pixel  = 1024;%native PLCXP200L resolution
     angular_span      = atand((left2right.metric/2)./distance2monitor)*2;%pixels per degree
     pixelperdegree    = left2right.pixel/angular_span;    
