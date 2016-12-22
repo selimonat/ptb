1095ac22db83a33b168e1ea0e7a174256d734445
leakp <lea.kampermann@gmail.com>
2016-12-22 17:04:38 +0100

    small corrections, RR=.5

--- seq_treatgen.m
+++ seq_treatgen.m
@@ -1,12 +1,13 @@
 %% testphase
 for nsub = 1:200;
     for csp            = 1:8;
+        others = setdiff(1:8,csp);
         cond_id        = [];
         ok = 0;
         while ~ok
-            ok =1 ;
+            ok = 1 ;
             %random
-            s = Shuffle([repmat(1:8,[1 4]) 9 9]);
+            s = Shuffle([repmat(others,[1 6]) ones(1,3)*csp ones(1,3)*9]);
             s = [0 s];
             %constraints
             % never two ucs after each other
@@ -18,27 +19,33 @@ for nsub = 1:200;
             if all(find(s==9)> 3)
                 ok = 0;
             end
-            %second ucs not in the last 8 trials
-            if any(find(s==9) > length(s)-8)
+            %ucs not in the last 5 trials
+            if any(find(s==9) > length(s)-5)
                 ok = 0;
             end
+            %not too close to each other
             if any(diff(find(s==9))<8)
                 ok = 0;
             end
+% %             not too far away also
+% %              if any(diff(find(s==9))>15)
+% %                 ok = 0;
+% %             end
         end
         for n = 1:9;fprintf('%g: %g\n',n,sum(s == n));end        
         % put the data to format usable by the presentation script.
         seq2 = [];
         seq2.cond_id               = s;
         seq2.ucs                   = s == 9;
-        seq2.tonicpain             = seq_BalancedDist(s,[5 6 7 6]); %take as many isi possibilities as reps for conds
-        seq2.tonicpain(s ==9)      = Shuffle([5 6]); % because there is only two, it would not be distributed the same across sequences otherwise
-        seq2.tonicpain(s ==0)      = 5; % because there is only two, it would not be distributed the same across sequences otherwise
+        seq2.tonicpain             = seq_BalancedDist(s,[5 6 7]); %take as many isi possibilities as reps for conds
+%         seq2.tonicpain(s ==9)      = Shuffle([5 6 7 5.5 6.5]); % because there is only two, it would not be distributed the same across sequences otherwise
+        seq2.tonicpain(s ==0)      = 6; % because there is only two, it would not be distributed the same across sequences otherwise
 %         seq2.isi2                  = 6 - seq2.isi1;
 %         seq2.isi3                  = seq_BalancedDist(s,[.5 .75 1]);
 %         seq2.poststim              = 1 - seq2.isi3;
         seq2.ratepain              = zeros(length(s),1);
-        seq2.ratepain([1 17])      = 1;
+        seq2.ratepain(mod(1:length(s),floor(length(s)/3))==1)      = 1;
+        seq2.ratepain(end)         = 0;
         seq2.stim_id               = s;
         seq2.stim_id(s==9)         = csp;
         seq2.dist                  = MinimumAngle((seq2.stim_id-1)*45,(csp-1)*45);
@@ -66,7 +73,7 @@ for nsub = 1:200;
         while ~ok
             ok =1 ;
             %random
-            s = [0 2 2 Shuffle([ones(1,6) ones(1,6)*2]) 1 1]; % 1 = CSP 2 = CSN
+            s = [0 2 3 Shuffle([ones(1,5) ones(1,9)*2 ones(1,4)*3])]; % 1 = CSP 2 = CSN 3 = UCS
             %constraints
 %             % one of the first three trials has to be an ucs
 %             if all(find(s==3)> 3)
@@ -76,22 +83,29 @@ for nsub = 1:200;
             if any(diff(diff(s))==0)
                 ok = 0;
             end
+            % never more than 8 trials without reinforcement
+            if any(diff(find(s==3))>5)
+                ok = 0;
+            end
         end
         if nsub == 200;for n = 1:length(unique(s));fprintf('%g: %g\n',n,sum(s == n));end;end        
         % put the data to format usable by the presentation script.
         seq2 = [];
         seq2.cond_id               = s;
-        seq2.ucs                   = s == 1;
-        seq2.tonicpain             = seq_BalancedDist(s,[5 6 7 6]); %take as many isi possibilities as reps for conds
-%         seq2.tonicpain(s == 3)     = Shuffle([5 6 6 7]); % because there is only two, it would not be distributed the same across sequences otherwise
+        seq2.ucs                   = s == 3;
+        seq2.tonicpain             = seq_BalancedDist(s,[5 6 7 5.5 6.5]); %take as many isi possibilities as reps for conds
+%         seq2.tonicpain(s == 3)     = Shuffle([5 6 7 5.5 6.5]); % because there is an equal number, it would not be distributed the same across sequences, i.e. peoples otherwise
+        seq2.tonicpain(s == 0)     = 6; % because there 
 %         seq2.isi2                  = 6 - seq2.isi1;
 %         seq2.isi3                  = seq_BalancedDist(s,[.5 .75 1]);
 %         seq2.poststim              = 1 - seq2.isi3;
         seq2.ratepain              = zeros(length(s),1);
-        seq2.ratepain(1)           = 1;
+        seq2.ratepain(mod(1:length(s),floor(length(s)/2))==1)      = 1;
+        seq2.ratepain(end)         = 0; %is taken care of anyway in the experiment
         seq2.stim_id               = s;
         seq2.stim_id(s==1)         = csp;
         seq2.stim_id(s==2)         = csn;
+        seq2.stim_id(s==3)         = csp;
         seq2.dist                  = MinimumAngle((seq2.stim_id-1)*45,(csp-1)*45);
         seq2.tTrial                = length(seq2.cond_id);
         %ucs=500
@@ -101,7 +115,7 @@ for nsub = 1:200;
         seq2.CrossPosition        =  seq_feargen_fixcross(seq2);
         seq2.CrossPosition(1,:)   = seq2.CrossPosition(end,:);
         if nsub == 1
-            fprintf('seq is about %g minutes long and has %g trials...\n',(length(s)*(1.5+.85+5)+sum(seq2.tonicpain))/60,length(s));
+            fprintf('seq is about %g minutes long and has %g trials...\n',(length(s)*(1.5+.85+6+5)+sum(seq2.tonicpain))/60,length(s));
         end
         %
         seq(nsub,csp) = seq2;
