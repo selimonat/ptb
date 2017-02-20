a523b481d90204f55c1ed45e192c7e5fa0d89219
Niklas Wilming <niklaswilming@fastmail.fm>
2017-02-20 22:55:15 +0100

    Start cleaning up

--- seq_optseq22s.m
+++
@@ -1,7 +0,0 @@
-function [s]=seq_optseq22s(filename)
-%seq_optseq22s(filename)
-%
-%   Reads the opt2 sequence from FILENAME and returns a sequence vector.
-
-%read the optseq
-[time s  c d e] = textread(filename,'%f%f%s%s%s','delimiter','\t');
